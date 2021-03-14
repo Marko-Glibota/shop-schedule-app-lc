5ce36b1d3d8c2fadf4a7396be83972f07c1a3ac8
Marko Glibota <markoglibota@gmail.com>
2021-03-14 11:01:46 +0100

    Added shops to index and weekdays to show with helper method to format weekdays

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,2 +1,19 @@
 module ShopsHelper
+  def format_weekday_fr(int)
+    if int == 0
+      "Dimanche"
+    elsif int == 1
+      "Lundi"
+    elsif int == 2
+      "Mardi"
+    elsif int == 3
+      "Mercredi"
+    elsif int == 4
+      "Jeudi"
+    elsif int == 5
+      "Vendredi"
+    else
+      "Samedi"
+    end
+  end
 end
