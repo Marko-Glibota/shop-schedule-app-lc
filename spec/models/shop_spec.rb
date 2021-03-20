58fb691345d9882491186221d0b4bf3a1dfb22b9
Marko Glibota <markoglibota@gmail.com>
2021-03-20 08:26:58 +0100

    Add tests for shop model

---
+++ spec/models/shop_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Shop, type: :model do
+
+  it "is not valid without valid attributes" do
+    expect(Shop.new).to_not be_valid
+  end
+
+  it "is valid with a name attribute" do
+    shop = Shop.new(name: "Anything")
+    expect(shop).to be_valid
+  end
+end
