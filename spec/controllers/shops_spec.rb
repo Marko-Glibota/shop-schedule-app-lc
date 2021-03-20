048543a5b23b2b3a6a345fcd4043609f5344d531
Marko Glibota <markoglibota@gmail.com>
2021-03-20 11:20:56 +0100

    Add test for shop show page

--- spec/controllers/shops_spec.rb
+++ spec/controllers/shops_spec.rb
@@ -1,9 +1,32 @@
 require "rails_helper"
 
 RSpec.describe '/shops', type: :request do
+
   describe "GET #index" do
     it "renders a successful response" do
       get root_path
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #show' do
+    it 'renders a successful response' do
+      shop = Shop.new(name: "Anything")
+      shop.save
+      weekdays = -1
+      7.times do
+        schedule = Schedule.new(
+          weekday: weekdays + 1,
+          morning_opening_hour: '9:00:00',
+          morning_closing_hour: '12:00:00',
+          afternoon_opening_hour: '13:00:00',
+          afternoon_closing_hour: '18:00:00',
+          shop: shop
+        )
+        weekdays += 1
+        schedule.save
+      end
+      get shop_path(shop)
       expect(response).to be_successful
     end
   end
